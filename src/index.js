b2d0be4ec17b685a66e200ac39a69ffb1c1b085e
makssen <maks.sen.1903@mail.ru>
2021-02-07 16:33:02 +0300

    feat: add method for task typical-arrays-problems

--- src/index.js
+++ src/index.js
@@ -1,12 +1,23 @@
+exports.min = function min(array) {
+    if (array === undefined || array.length === 0) {
+        return 0;
+    } else {
+        return Math.min(...array);
+    }
 
-exports.min = function min (array) {
-  return 0;
 }
 
-exports.max = function max (array) {
-  return 0;
+exports.max = function max(array) {
+    if (array === undefined || array.length === 0) {
+        return 0;
+    } else {
+        return Math.max(...array);
+    }
 }
-
-exports.avg = function avg (array) {
-  return 0;
+exports.avg = function avg(array) {
+    if (array === undefined || array.length === 0) {
+        return 0;
+    } else {
+        return array.reduce((num, item) => num + item) / array.length;
+    }
 }
